88a777edac96779e3192d06ddb04ba27609d1869
yoratoni <71235233+yoratoni@users.noreply.github.com>
2023-07-23 00:29:31 +0200

    WIP on caret

--- src/components/Matrix/index.tsx
+++ src/components/Matrix/index.tsx
@@ -1,78 +1,84 @@
 import { useEffect, useRef, useState } from "react";
 
 import useIpAddress from "@/hooks/useIpAddress";
-import useWindowSize from "@/hooks/useWindowSize";
+import useTextAreaAutoSize from "@/hooks/useTextAreaAutoSize";
 
 
 export default function Matrix() {
-    const { width } = useWindowSize();                                  // Window width
     const ipAddress = useIpAddress();                                   // IP address
 
-    const [username, setUsername] = useState("[loading..]");            // Username
-    const [caretOffset, setCaretOffset] = useState(0);                  // Offset of caret from left
-    const [caret, setCaret] = useState("█");                            // Caret character (blinking)
+    const [matrix, setMatrix] = useState({
+        username: "",
 
-    const [persistentTextIndex, setPersistentTextIndex] = useState(0);  // Index of persistent text
-    const [input, setInput] = useState("");                             // User input
+        caretEnabled: true,
+        caretOffset: 0,
+        caret: "█",
 
-    const [charWidth, setCharWidth] = useState(0);                      // Width of a single character
-    const [charPerLine, setCharPerLine] = useState(0);                  // Number of characters per line
+        value: "",
 
-    const usernameRef = useRef<HTMLDivElement>(null);
-    const textAreaRef = useRef<HTMLTextAreaElement>(null);
+        usernameRef: useRef<HTMLDivElement>(null),
+        textAreaRef: useRef<HTMLTextAreaElement>(null),
+        caretAreaRef: useRef<HTMLTextAreaElement>(null)
+    });
+
+    // Auto-size text area & caret area
+    useTextAreaAutoSize(matrix.textAreaRef.current, matrix.value);
+    useTextAreaAutoSize(matrix.caretAreaRef.current, matrix.caret);
 
     // Generate formatted username
     useEffect(() => {
-        setUsername(`[${ipAddress}@SEVRIL]$`);
+        setMatrix(matrix => ({
+            ...matrix,
+            username: `[${ipAddress}@SEVRIL]$`
+        }));
     }, [ipAddress]);
 
     // Caret blink
     useEffect(() => {
         const interval = setInterval(() => {
-            setCaret(caret === "█" ? " " : "█");
-        }, 500);
+            if (
+                !matrix.textAreaRef.current?.selectionStart ||
+                (
+                    matrix.textAreaRef.current?.selectionStart &&
+                    matrix.value.length > matrix.textAreaRef.current?.selectionStart
+                )
+            ) {
+                // Removes the custom caret while not at the end of the user input
+                setMatrix(matrix => ({
+                    ...matrix,
+                    caret: ""
+                }));
+
+                // Enable normal caret by disabling the custom caret
+                setMatrix(matrix => ({
+                    ...matrix,
+                    caretEnabled: false
+                }));
+            } else {
+                setMatrix(matrix => ({
+                    ...matrix,
+                    caretEnabled: true,
+                    caret: matrix.caret === "█" ? "" : "█"
+                }));
+            }
+        }, 450);
 
         return () => clearInterval(interval);
-    }, [caret]);
+    }, [matrix.caretEnabled, matrix.caret]);
 
     // Measure caret offset
     useEffect(() => {
-        const caretPosition = textAreaRef.current?.selectionStart || 0;
-        setCaretOffset(username.length + caretPosition + 1);
-    }, [username, input, textAreaRef.current?.selectionStart]);
+        const caretPosition = matrix.textAreaRef.current?.selectionStart || 0;
 
-    // Measure character width from username
-    useEffect(() => {
-        const handleResize = () => {
-            if (usernameRef.current) {
-                setCharWidth(Math.ceil(usernameRef.current.clientWidth / username.length));
-            }
-        };
-
-        handleResize();
-
-        window.addEventListener("resize", handleResize);
-
-        return () => window.removeEventListener("resize", handleResize);
-    }, [username]);
-
-    // Measure max number of characters per line
-    useEffect(() => {
-        const handleResize = () => {
-            const textArea = document.querySelector("textarea");
-
-            if (textArea && charWidth > 0) {
-                setCharPerLine(Math.floor(textArea.clientWidth / charWidth));
-                console.log(Math.floor(textArea.clientWidth / charWidth) - username.length + 3);
-            }
-        };
-
-        handleResize();
-
-        window.addEventListener("resize", handleResize);
-
-        return () => window.removeEventListener("resize", handleResize);
-    }, [width]);
+        setMatrix(matrix => ({
+            ...matrix,
+            caretOffset: matrix.username.length + caretPosition
+        }));
+    }, [
+        matrix.username,
+        matrix.value,
+        matrix.textAreaRef.current?.selectionStart
+    ]);
 
     // Handle user input
     const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -83,53 +89,48 @@ export default function Matrix() {
             return;
         }
 
-        // Prevent user from deleting persistent text
-        if (value.length < persistentTextIndex) {
-            return;
-        }
-
-        setInput(value);
-    };
-
-    const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-        if (e.key === "Enter") {
-            e.preventDefault();
-
-            // Update persistent text index
-            setPersistentTextIndex(input.length);
-        }
+        setMatrix(matrix => ({
+            ...matrix,
+            value: value
+        }));
     };
 
     return (
         <div className="relative w-full h-full px-6 py-5 overflow-hidden sm:text-2xl max-sm:text-xl selection:bg-primary-default selection:bg-opacity-[0.6] selection:text-white">
-            <div className="absolute top-5 left-6 text-primary-default" ref={usernameRef}>
-                {username}
+            <div className="absolute top-5 left-6 text-primary-default" ref={matrix.usernameRef}>
+                {matrix.username}
             </div>
 
             <textarea
-                className="relative z-10 w-full h-full overflow-hidden tracking-wide bg-transparent outline-none resize-none text-primary-default text-glitch caret-transparent"
+                className={`
+                    relative z-10 w-full overflow-hidden tracking-wide bg-transparent
+                    outline-none resize-none text-primary-default text-glitch
+                    ${matrix.caretEnabled ? "caret-transparent" : ""}
+                `}
                 style={{
-                    textIndent: `${username.length + 1}ch`,
+                    textIndent: `${matrix.username.length + 1}ch`,
                 }}
-                ref={textAreaRef}
+                ref={matrix.textAreaRef}
                 autoComplete="off"
                 autoCorrect="off"
                 autoCapitalize="off"
                 spellCheck={false}
-                value={input}
+                value={matrix.value}
+                rows={1}
                 autoFocus
                 onChange={handleTextChange}
-                onKeyDown={handleKeyDown}
             />
 
             <textarea
-                className="absolute z-0 overflow-hidden bg-transparent outline-none resize-none select-none bottom-5 right-6 left-6 top-5"
+                className="absolute z-0 -ml-[1px] overflow-hidden tracking-wide caret-transparent bg-transparent outline-none resize-none select-none right-6 left-6 top-5"
+                ref={matrix.caretAreaRef}
                 autoComplete="off"
                 autoCorrect="off"
                 autoCapitalize="off"
                 spellCheck={false}
+                rows={1}
                 readOnly
-                value={"\xa0".repeat(caretOffset) + caret}
+                value={"\xa0".repeat(matrix.caretOffset) + matrix.caret}
             />
         </div>
     );
---
+++ src/hooks/useTextAreaAutoSize.ts
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+
+export default function useTextAreaAutoSize(
+    textAreaRef: HTMLTextAreaElement | null,
+    value: string
+) {
+    useEffect(() => {
+        if (textAreaRef) {
+            textAreaRef.style.height = "0px";
+
+            const scrollHeight = textAreaRef.scrollHeight;
+            textAreaRef.style.height = scrollHeight + "px";
+        }
+    }, [textAreaRef, value]);
+};
