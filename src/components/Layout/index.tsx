e1c679a5261cdd179b97586c3388bf9077d5ce75
yoratoni <71235233+yoratoni@users.noreply.github.com>
2023-07-26 08:32:14 +0200

    Removed terminal, adding it later

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -6,7 +6,7 @@ import Header from "@/components/Layout/Header";
 
 export default function Layout({ children }: { children: ReactNode; }) {
     return (
-        <div className="flex flex-col w-screen h-screen crt bg-main text-primary-default">
+        <div className="flex flex-col w-screen min-h-screen crt bg-main text-primary-default">
             <Header />
 
             <main className="relative w-full h-full overflow-hidden border">
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,10 @@
 import Layout from "@/components/Layout";
-import Matrix from "@/components/Matrix";
 
 
 export default function Home() {
     return (
         <Layout>
-            <Matrix />
+
         </Layout>
     );
 }
